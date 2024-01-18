9b8010143f30cb0850a329b9ff1adece2693dd2b
Lucas Mozzer <mozzerdeoliveira@gmail.com>
2024-01-18 18:54:43 -0300

    first commit

---
+++ queue-manage/src/main/java/com/br/queuemanage/queuemanage/QueueManageApplication.java
@@ -0,0 +1,13 @@
+package com.br.queuemanage.queuemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueManageApplication.class, args);
+	}
+
+}
---
+++ queue-manage/src/test/java/com/br/queuemanage/queuemanage/QueueManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.queuemanage.queuemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
